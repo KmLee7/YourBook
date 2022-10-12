f4b79aacc3318b29bd135e27bc531d9add1979dd
“Kyungmin <rudalslee7@gmail.com>
2022-10-12 00:14:28 -0400

    add css for homepage

--- YourBook/frontend/src/components/Navigation/index.js
+++ YourBook/frontend/src/components/Navigation/index.js
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-// import ProfileButton from './ProfileButton';
-// import LoginFormModal from '../LoginFormModal';
+import ProfileButton from './ProfileButton';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 import { logout } from '../../store/session';
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
   let sessionLinks;
-  if (sessionUser) {
+  if (!sessionUser) {
+    return (
+      sessionLinks = (
+      
+        <div className='home-page'>
+          <div className='left-container'>
+              <div id='facebook-logo'>facebook</div>
+            <h2 id='home-page-info'>Connect with friends and the world around you on Facebook</h2>
+          </div>
+          <div className='right-container'>
+            <input
+            id='login-email'
+            placeholder='Email or phone number' 
+            type='email'
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+            />
+            <br />
+            <input
+            id='login-password'
+            placeholder='Password' 
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+            <br />
+          <button className='loginButton'>Log In</button>
+          <hr className='borderline'></hr>
+          <div style={{textAlign: 'center'}}>
+            <SignupFormModal />
+          </div>
+          </div>
+        </div>
+      
+      )
+    );
     // sessionLinks = (
-    //   // <ProfileButton user={sessionUser} />
+    //   <ProfileButton user={sessionUser} />
     // );
-    sessionLinks = ( 
-    <button onClick={() => {dispatch(logout())}}>Logout</button>
-    );
   } else {
-    sessionLinks = (
-      <>
-        {/* <LoginFormModal /> */}
-        <NavLink to="/signup">Sign Up</NavLink>
-        <br></br>
-        <NavLink to="/login">Login</NavLink>
-      </>
-    );
+    return (
+      sessionLinks = ( 
+        <button onClick={() => {dispatch(logout())}}>Logout</button>
+      )
+    )
+    
   }
-
-  return (
-    <ul>
-        <NavLink exact to="/">Home</NavLink>
-        <br></br>
-        {sessionLinks}
-    </ul>
-  );
 }
 
 export default Navigation;
--- YourBook/frontend/src/components/SignupFormModal/SignupForm.js
+++ YourBook/frontend/src/components/SignupFormModal/SignupForm.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
-import "./SignupFormPage.css";
+import "./SignupForm.css";
 
-function SignupFormPage() {
+function SignupForm() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [birthday, setBirthday] = useState("");
@@ -15,8 +13,6 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
-
-  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -126,4 +122,4 @@ function SignupFormPage() {
   );
 }
 
-export default SignupFormPage;
+export default SignupForm;
