5d3b3d394cdf3f4ed3d01850666aba0e8bdb186f
“Kyungmin <rudalslee7@gmail.com>
2022-12-09 21:06:40 -0500

    fix comment input css

--- frontend/src/components/Comments/index.js
+++ frontend/src/components/Comments/index.js
@@ -15,9 +15,16 @@ function Comments({ postId }) {
   );
   const posts = useSelector((state) => state.entities.posts);
   // console.log(posts);
-  let currentUser = useSelector((state) => {
-    return state.entities.users[id];
-  });
+  // let currentUser = useSelector((state) => {
+  //   return state.entities.users[id];
+  // });
+  // console.log(currentUser);
+  const allUsers = Object.values(useSelector((state) => state.entities.users));
+
+  const currentUser = allUsers.map((user) =>
+    user.id === currentUserId ? user.first_name + " " + user.last_name : ""
+  );
+  console.log(currentUser);
   // console.log(comments, "this comment");
   // console.log(tempcurrentUser, "this current user");
   const postList = Object.values(posts);
@@ -40,16 +47,24 @@ function Comments({ postId }) {
   return (
     <>
       <form className="comment-form" onSubmit={handleSubmit}>
-        <div>Write a comment</div>
         <input
-          style={{ width: "50px" }}
+          style={{
+            width: "97%",
+            height: "30px",
+            border: "none",
+            borderRadius: "20px",
+            backgroundColor: "rgb(244, 242, 242)",
+            textIndent: "5px",
+            fontSize: "18px",
+            marginBottom: "10px",
+          }}
           id="comment-content"
           type="text"
-          placeholder="Write a comment"
+          placeholder="Write a comment..."
           value={body}
           onChange={(e) => setBody(e.target.value)}
         />
-        <button type="submit">Comment</button>
+        {/* <button type="submit">Comment</button> */}
       </form>
     </>
   );
