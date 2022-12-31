d3f0c5d65fa295437f8b8c07bd67655b96e94a1e
“Kyungmin <rudalslee7@gmail.com>
2022-12-30 19:31:51 -0500

    add friends migration2

--- db/migrate/20221231002059_create_friends.rb
+++ db/migrate/20221231002059_create_friends.rb
@@ -1,7 +1,10 @@
 class CreateFriends < ActiveRecord::Migration[7.0]
   def change
     create_table :friends do |t|
-
+      t.integer :sender_id, null: false, foreign_key: {to_table: users}
+      t.integer :receiver_id, null: false, foreign_key: {to_table: users}
+      t.boolean :accept, default: false, null: false
+      t.boolean :decline, default: false, null: false 
       t.timestamps
     end
   end
